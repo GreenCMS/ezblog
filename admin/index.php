e5d655bac960208b636d9101f7945cb3b3fd0281
ZTS <admin@zts1993.com>
2013-03-23 14:35:13 +0800

    Update /admin

--- admin/index.php
+++ admin/index.php
@@ -4,7 +4,7 @@ if (! isset ( $_SESSION ['gen'] )) {
 	header ( "Location:login.php" ); //重新定向到其他页面
 	exit ();
 } else {
-	header ( "Location:home.php" ); //重新定向到其他页面
+	header ( "Location:index.html"); //重新定向到其他页面
 	exit ();
 
 }
